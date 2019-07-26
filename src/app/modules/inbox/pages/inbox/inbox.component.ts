5a4c7ba0b6fd7d4394f371c06d611ad9a4be95e7
ubuntu <lmshanmugam1991@gmail.com>
2019-07-26 16:13:57 +0530

    feature: inbox design based on sample data completed

--- src/app/modules/inbox/pages/inbox/inbox.component.ts
+++ src/app/modules/inbox/pages/inbox/inbox.component.ts
@@ -12,4 +12,207 @@ export class InboxComponent implements OnInit {
   ngOnInit() {
   }
 
+  userList = [
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    },
+    {
+      userName: "David James",
+      designation: "12131123 - Performance",
+      date: new Date("2019-07-02"),
+      info: "lorem ipsum door sit lorem ipsum door"
+    }
+  ]
+
+  chartList = [
+    {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    },
+    {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    },
+    {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    },
+    {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    },
+    {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    },
+    {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    }
+    , {
+      userName: "Baker",
+      message: "Hi, got an issue on my process",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Tsf",
+      message: "lorem ipsum door sit lorem ipsum door sit sldkflkds ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: false
+    },
+    {
+      userName: "Me",
+      message: "lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit loresdflkdsf rem ipsum door sit lorem ipsum door sit lorem ipsum door sit lorem ipsum door sit",
+      date: new Date("2019-07-02"),
+      isme: true
+    }
+  ]
 }
